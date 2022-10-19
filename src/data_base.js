f6a8c8da405d7472f50a30898161451392a39701
yasinabdmahmood <abd181103@gmail.com>
2022-10-19 23:24:54 +0300

    Add images

---
+++ src/data_base.js
@@ -0,0 +1,180 @@
+ const ar1=[
+    {
+        src:'./assets/images/dumplings.jpg',
+        name:'"Homemade" dumplings',
+        discription:'Vareniki with mashed potatoes and fried chanterelles',
+        price:4.5,
+        order:0
+    },
+    {
+        src:'./assets/images/soup.jpg',
+        name:'Soup',
+        discription:'Potatoes, carrots, onions, green onions',
+        price:4.3,
+        order:0
+    },
+    {
+        src:'./assets/images/burger.jpg',
+        name:'Burger',
+        discription:'Beef, lettuce, pickle, tomato, soft bun',
+        price:5.9,
+        order:0
+    },
+    {
+        src:'./assets/images/borscht.jpg',
+        name:'Borscht',
+        discription:'Beetroot borsch with bacon and donuts',
+        price:4.6,
+        order:0
+    },
+    {
+        src:'./assets/images/fishPancake.jpg',
+        name:'Fish pancakes',
+        discription:'Beef, lettuce, pickle, tomato, soft bun',
+        price:6.5,
+        order:0
+    },
+    {
+        src:'./assets/images/hotRolls.jpg',
+        name:'Hot rolls',
+        discription:'Temporarily unavailable',
+        price:4.9,
+        order:0
+    },
+ ]
+ 
+ const ar2=[
+    {
+        src:'./assets/images/avogado.jpg',
+        name:'Avocado salad with feta',
+        discription:'Ripe avocado, feta cheese, olives and spinach salad',
+        price:4.9,
+        order:0
+    },
+    {
+        src:'./assets/images/camembert.jpg',
+        name:'Camembert salad',
+        discription:'Baked Camembert cheese with fresh vegetables',
+        price:4.5,
+        order:0
+    },
+    {
+        src:'./assets/images/greekSalad.jpg',
+        name:'Greek saladger',
+        discription:'Tomato, cucumber, olives, feta, olive oil',
+        price:5.5,
+        order:0
+    },
+    {
+        src:'./assets/images/Guacamole.jpg',
+        name:'Guacamole',
+        discription:'Mexican guacamole with nachos',
+        price:3.9,
+        order:0
+    },
+   
+ ]
+ const ar3=[
+    {
+        src:'./assets/images/RaspberryMousse.jpg',
+        name:'Raspberry mousse',
+        discription:'Raspberry mousse with cream and raspberry syrup',
+        price:3.25,
+        order:0
+    },
+    {
+        src:'./assets/images/VanillaIceCream.jpg',
+        name:'SoVanilla ice creamup',
+        discription:'Vanilla ice cream with chocolate filling',
+        price:2.5,
+        order:0
+    },
+    {
+        src:'./assets/images/BrownieWithRaspberries.jpg',
+        name:'Brownie with raspberries',
+        discription:'Chocolate brownie with fresh raspberries',
+        price:4.75,
+        order:0
+    },
+    {
+        src:'./assets/images/Cheesecake.jpg',
+        name:'Cheesecake',
+        discription:'Temporarily unavailable',
+        price:4.99,
+        order:0
+    },
+    {
+        src:'./assets/images/BerryIceCream.jpg',
+        name:'Berry ice cream',
+        discription:'Natural ice cream made from berries',
+        price:2.25,
+        order:0
+    },
+    
+ ]
+ const ar4=[
+    {
+        src:'./assets/images/FullEnglishBreakfast.jpg',
+        name:'Full English breakfast',
+        discription:'Fried eggs, bacon, toast, cherry tomatoes',
+        price:6.99,
+        order:0
+    },
+    {
+        src:'./assets/images/Oatmeal.jpg',
+        name:'Oatmeal',
+        discription:'Oatmeal with seasonal berries and berry syrup',
+        price:3.25,
+        order:0
+    },
+    {
+        src:'./assets/images/Cheesepancakeswithstrawberryjam.jpg',
+        name:'Cheese pancakes with strawberry jam',
+        discription:'Fresh cottage cheese pancakes with strawberry jam and sour cream',
+        price:4.6,
+        order:0
+    },
+    {
+        src:'./assets/images/AvocadoToast.jpg',
+        name:'Avocado toast',
+        discription:'Rye toast with avocado and boiled egg',
+        price:2.15,
+        order:0
+    },
+    {
+        src:'./assets/images/Catfishonpumpkinpuree.jpg',
+        name:'Catfish on pumpkin puree',
+        discription:'Catfish fillet baked with young potatoes. Served with salad and pumpkin puree',
+        price:6.5,
+        order:0
+    },
+   
+ ]
+ const catagories=[
+    {
+        id:0,
+        name:"HOT MEALS",
+        src:'./assets/images/hotMeals.jpg',
+        list:ar1,
+    },
+    {
+        id:1,
+        name:"SALADS",
+        src:'./assets/images/saladjpg.jpg',
+        list:ar2,
+    },
+    {
+        id:2,
+        name:"DESSERTS",
+        src:'./assets/images/desserts.jpg',
+        list:ar3,
+    },
+    {
+        id:3,
+        name:"BEAKFASTS",
+        src:'./assets/images/breackfasts.jpg',
+        list:ar4,
+    },
+    
+]
+export default catagories;
