1df708a9cf7a797f12b42c9d0165d253b934bb2f
yasinabdmahmood <abd181103@gmail.com>
2022-10-20 00:36:01 +0300

    Add redux to the project

---
+++ src/redux/configureStore.js
@@ -0,0 +1,19 @@
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import logger from 'redux-logger';
+import  mealsReducer  from './dataReduser';
+
+const rootReducer = combineReducers({
+  meals: mealsReducer,
+  
+
+});
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: ['getCountries//fulfilled', 'getDetials//fulfilled'],
+    },
+  }).concat(logger),
+});
+
+export default store;
---
+++ src/redux/dataReduser.js
@@ -0,0 +1,17 @@
+import { createSlice} from '@reduxjs/toolkit';
+import catagories from './data_base';
+
+
+
+
+const initialState = catagories;
+
+export const mealsReducer = createSlice({
+  name: 'meals',
+  initialState,
+  reducers : {
+
+  }
+});
+//export const {  } = mealSlice.actions;
+export default mealsReducer.reducer;
