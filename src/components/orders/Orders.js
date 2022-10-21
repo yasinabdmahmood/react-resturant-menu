d3b27be8a4afc120384d94bf7a203d57cf3404ee
yasinabdmahmood <abd181103@gmail.com>
2022-10-21 12:00:54 +0300

    Add empty order view in Orders section

--- src/components/orders/Orders.js
+++ src/components/orders/Orders.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import Meal from '../Meal/Meal';
 import './Orders.scss';
 import {useSelector} from 'react-redux'
 
 function Orders(props) {
+  const navigate = useNavigate();
+  const emptyOrder = (
+    <div className='empty-order'>
+      <h2>EMPTY ORDER</h2>
+      <div>
+      <button onClick={() => navigate(-1)}>Back to menu</button>
+      </div>
+      
+    </div>
+  )
   const chooseOrderedFoods =(state) => {
     const arr = [];
     state.forEach(element => {
@@ -17,9 +28,9 @@ function Orders(props) {
   }
   const orderedMeal = useSelector(state => chooseOrderedFoods(state.meals))
   return (
-    <div className="meals-container">
+    <div className="meals-container">      
       <h1>My order</h1>
-      {orderedMeal.map((el) => <Meal meal={el} CatagoryId={el.CatagoryId} />)}
+      {orderedMeal.length>0? orderedMeal.map((el) => <Meal meal={el} CatagoryId={el.CatagoryId} />) : emptyOrder}
     </div>
   );
 }
