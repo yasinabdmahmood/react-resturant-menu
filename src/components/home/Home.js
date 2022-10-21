34dfda8e6571d8e52e8b59c743bf992deda5f1e4
yasinabdmahmood <abd181103@gmail.com>
2022-10-21 03:08:14 +0300

    Complete Show order page functionalities

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,18 +1,44 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes,Link,useNavigate,useLocation  } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import Types from '../types/Types';
 import Orders from '../orders/Orders';
 import Meals from '../meals/Meals';
+import goBack from '../../assets/images/arrow-left.svg'
 import './Home.scss';
 
 function Home() {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const types = useSelector((state) => state.meals);
+  const showOrder = (input) => {
+    let bool = false;
+    input.forEach(element => {
+      element.list.forEach(el=>{
+        if(el.order>0){
+          bool=true;
+        }
+      })
+    });
+    return bool
+  }
   return (
     <div className="home-page">
+      <img
+      className={location.pathname==='/'? 'hide':'go-Back'}
+       src={goBack}
+       alt='go-back'
+       onClick={() => navigate(-1)} />
       <Routes>
         <Route exact path="/" element={<Types />} />
         <Route exact path="/meals" element={<Meals />} />
         <Route exact path="/orders" element={<Orders />} />
       </Routes>
+      <div className={showOrder(types)? 'order-display':'hide'} >
+        <Link to="/orders">
+        <span>Show my order</span>
+        </Link>        
+      </div>
     </div>
   );
 }
--- src/components/orders/Orders.js
+++ src/components/orders/Orders.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import Meal from '../Meal/Meal';
 import './Orders.scss';
+import {useSelector} from 'react-redux'
 
 function Orders(props) {
+  const chooseOrderedFoods =(state) => {
+    const arr = [];
+    state.forEach(element => {
+      element.list.forEach(el=>{
+        if(el.order>0){
+          arr.push({...el,CatagoryId:element.id})
+        }
+      })
+    });
+    return arr
+  }
+  const orderedMeal = useSelector(state => chooseOrderedFoods(state.meals))
   return (
-    <div>
-      <h1>Orders</h1>
+    <div className="meals-container">
+      <h1>My order</h1>
+      {orderedMeal.map((el) => <Meal meal={el} CatagoryId={el.CatagoryId} />)}
     </div>
   );
 }
